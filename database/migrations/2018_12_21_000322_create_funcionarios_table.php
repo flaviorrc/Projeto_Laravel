ba4ce9d77376895fde58702ccbc20143a5d56a38
Flavio Costa <flavio.costa@acupula.com.br>
2018-12-20 22:09:40 -0200

    migrations

---
+++ database/migrations/2018_12_21_000322_create_funcionarios_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFuncionariosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('funcionarios', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->unique();
+            $table->string('email', 150);
+            $table->string('telefone', 20)->nullable();
+            $table->integer('cpf')->nullable();
+            $table->integer('empresa')->unsigned();
+            $table->foreign('empresa')->references('id')->on('empresas')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('funcionarios');
+    }
+}
